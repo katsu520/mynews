8ac0fdc2ab2c79fd80990cdce9f82e91ae9f671d
”名前” <>
2019-02-26 01:33:16 +0900

    メッセージファイルです

---
+++ resources/views/admin/profile/create.blade.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+@extends('layouts.profile')
+
+@section('title', 'Myプロフィール')
+
+@section('content')
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+
+  </body>
+</html>
+
+  <div class="container">
+    <div class="row">
+        <div class="col-md-8 mx-auto">
+            <h2>Myプロフィール</h2>
+        </div>
+    </div>
+  </div>
+@endsection
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" contact="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+
+    <title>MyNews</title>
+  </head>
+  <body>
+    <h1>Myニュース作成画面</h1>
+  </body>
+</html>
