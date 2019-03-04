fb16417c83c6bbfaaecf1a325fba012825035732
”名前” <>
2019-03-05 00:49:17 +0900

    second commit

--- resources/views/admin/profile/edit.blade.php
+++ resources/views/admin/profile/edit.blade.php
@@ -2,9 +2,12 @@
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
-    <title></title>
+    <meta http-equiv="X-UA-Compatible" contact="IE=edge">
+    <meta name="viewport" content="width=device">
+
+    <title>MyProfile</title>
   </head>
   <body>
-
+    <h1>MyProfile作成画面</h1>
   </body>
 </html>
