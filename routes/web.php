d429144d7f2c432e40e25a7cd46e9912786ea3a9
”名前” <>
2019-03-06 00:46:55 +0900

    second commit

--- routes/web.php
+++ routes/web.php
@@ -23,12 +23,13 @@ Route::group(['prefix' => 'admin'], function() {
   Route::get('news/edit', 'Admin\NewsController@edit')->middleware('auth');
   Route::post('news/edit', 'Admin\NewsController@update')->middleware('auth');
   Route::get('news/delete', 'Admin\NewsController@delete')->middleware('auth');
+  Route::get('profile/create', 'Admin\ProfileController@add')->middleware('auth');
+  Route::get('profile/edit', 'Admin\ProfileController@edit')->middleware('auth');
 });
 
 //Route::get('XXX', 'Admin\AAAController@bbb');
 
-Route::get('admin/profile/create', 'Admin\ProfileController@add');
-Route::get('admin/profile/edit', 'Admin\ProfileController@edit');
+
 
 Auth::routes();
 
