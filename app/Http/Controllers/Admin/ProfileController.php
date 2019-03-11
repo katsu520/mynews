c90d6af0c85570e28d7b9e0f22ee413c2351769a
”名前” <>
2019-03-11 23:34:43 +0900

    sixth commit

--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -6,6 +6,9 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Profile;
 
+use App\profile_History;
+use Carbon\Carbon;
+
 class ProfileController extends Controller
 {
     //
@@ -18,8 +21,8 @@ class ProfileController extends Controller
 
       $profile =new Profile;
       $form = $request->all();
+      unset($form['_token']);
 
-      
 
       $profile->fill($form);
       $profile->save();
@@ -28,21 +31,26 @@ class ProfileController extends Controller
     }
     public function edit(Request $request){
 
-      $profile = Profile::find($request->id);
-      if(empty($profile)){
-        abort(404);
-      }
-      return view('admin.profile.edit', ['profile_form' => $profile]);
+      $profile = Profile::orderBy('created_at', 'desc')->first();
+
+      $profilehistories = ProfileHistory::all();
+
+      return view('admin.profile.edit', ['profile_form' => $profile, 'profilehistories' => $profilehistories]);
     }
     public function update(Request $request){
 
       $this->validate($request, Profile::$rules);
-      $profile = Profile::find($request->id);
+      $profile = new Profile;
 
       $profile_form = $request->all();
       unset($profile_form['_token']);
 
       $profile->fill($profile_form)->save();
+
+      $history = new ProfileHistory();
+      $history->profile_id = $profile->id;
+      $history->edited_at = Carbon::now();
+      $history->save();
 
       return redirect('admin/profile/edit');
     }
--- app/Profile.php
+++ app/Profile.php
@@ -20,7 +20,7 @@ class Profile extends Model
 
     public function histories()
     {
-      return $this->hasMany('App\History');
+      return $this->hasMany('App\ProfileHistory');
 
     }
 }
